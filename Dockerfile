2e21daf2a1233b5ad4a3f6471c6abdc940f9a55a
bish0p <bish0pd2138@gmail.com>
2024-03-14 19:36:20 +0800

    Created a dockerfile, added docker-compose, .env.example.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.11-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+
+WORKDIR /referral_app
+
+COPY . .
+
+RUN pip install --upgrade pip &&  \
+    pip install poetry \
+
+RUN poetry config installer.max-workers 10 &&  \
+    poetry install
--- app/config.py
+++ app/config.py
@@ -23,20 +23,6 @@ class Settings(BaseSettings):
     def DB_URI(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
-
-    @property
-    def TEST_DB_URI(self):
-        return (
-            f"postgresql+asyncpg://{self.TEST_DB_USER}:"
-            f"{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:"
-            f"{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
-        )
-
     REDIS_HOST: str
     REDIS_PORT: int
     CACHE_TTL: int = 30
